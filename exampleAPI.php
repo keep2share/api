48101dbd9ee53dd4877d2c79c52c9e0990948ce8
keep2share <sup@keep2share.com>
2013-09-17 18:36:21 +0700

    Update exampleAPI.php

--- exampleAPI.php
+++ exampleAPI.php
@@ -3,16 +3,45 @@
 include "Keep2ShareAPI.php";
 
 $api = new Keep2ShareAPI();
-$api->username='test_api@k2s.cc';
-$api->password='testapik2scc';
+$api->username='your_account';
+$api->password='your_password';
 
-//getUploadFormData
-var_dump($api->getUploadFormData());
+//getFilesList
+var_dump($api->getFilesList());
+/*
+array(3) {
+  ["status"]=>
+  string(7) "success"
+  ["code"]=>
+  int(200)
+  ["files"]=>
+  array(1) {
+    [0]=>
+    array(4) {
+      ["id"]=>
+      string(13) "522f0bf5672f8"
+      ["name"]=>
+      string(9) "README.md"
+      ["is_available"]=>
+      bool(true)
+      ["size"]=>
+      string(4) "2857"
+    }
+  }
+}
+*/
 
-
-//get File List
-$hash=$api->getFilesList();
-
-
+//uploadFile
+var_dump($api->uploadFile('file_path'));
+/*
+object(stdClass)#2 (3) {
+  ["user_file_id"]=>
+  string(13) "5238354a724c3"
+  ["status"]=>
+  string(7) "success"
+  ["status_code"]=>
+  int(200)
+}
+*/
 
 ?>
